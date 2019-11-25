560652c79ac94dba06dff7eb5cde57005521629c
NATHANIEL ZAJAC <njzajac@email.sc.edu>
2019-11-25 18:06:04 -0500

    added sheetjs stuff

--- src/CSVImport.js
+++ src/CSVImport.js
@@ -4,6 +4,9 @@ import React, { Component } from "react"; //gives component class on top of reac
 //import Dropzone from "./dropzone";
 import "./csvImport.css";
 import Papa from "papaparse";
+import { JsonToTable } from "react-json-to-table";
+import { CSVLink, CSVDownload } from "react-csv";
+import XLSX from 'xlsx';
 //import { CSVReader } from 'react-papaparse';
 
 class FileReader extends React.Component {
@@ -11,7 +14,7 @@ class FileReader extends React.Component {
     super();
     this.state = {
       csvfile: undefined
-    };
+      };
     this.updateData = this.updateData.bind(this);
   }
 
@@ -21,23 +24,32 @@ class FileReader extends React.Component {
     });
   };
 
-  importCSV = () => {
+ /* importCSV = () => {
     const { csvfile } = this.state;
     Papa.parse(csvfile, {
       complete: this.updateData,
-      header: true
+        header: true,
+        dynamicTyping: true,
+        skipEmptyLines: true,
+        transformHeader: header =>
+            header
+                .toLowerCase()
+                .replace(/\W/g, '_')
     });
-  };
+  };*/
+
+
 
   updateData(result) {
-    var data = result.data;
-    console.log(data);
+      var data = result.data;
+      console.log(data);
+      return data;
   }
 
   render() {
     console.log(this.state.csvfile);
-    return (
-      <div className="csvImport">
+      return (
+        <div className="csvImport">
         <h2>Import CSV File!</h2>
         <input
           className="csv-input"
@@ -48,15 +60,18 @@ class FileReader extends React.Component {
           name="file"
           placeholder={null}
           onChange={this.handleChange}
-        />
+              />
         <p />
-        <button onClick={this.importCSV}> Upload now!</button>
+              <button onClick={this.importCSV}> Upload now!</button>
       </div>
     );
   }
 }
 
 export default FileReader;
+
+// <input type="file" onchange="parseExcelFile1(this)">
+
 
 // We pass onDrop function and accept prop to the component. It will be used as initial params for useDropzone hook
 /*    return (
