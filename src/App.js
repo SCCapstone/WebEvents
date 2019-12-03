6fa29d36f338894593e84f568c25e06d6dc3a578
Edwards97 <42946380+Edwards97@users.noreply.github.com>
2019-12-02 21:16:37 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from "react";
 import "./App.css";
 import CSVImport from "./CSVImport";
 import TitleBar from "./TitleBar";
+import SheetJSApp from "./sheet";
+import RequestServer from "./RequestServer";
+import FileUpload from "./FileUpload";
 import Options from "./Options";
 
 const App = () => {
   return (
     <div className="App">
-        <TitleBar />
-        <Options />
-        <CSVImport />
-        
+          <TitleBar />
+          <CSVImport />
+          <SheetJSApp />
+          <RequestServer />
+          <FileUpload />
+          <Options />
     </div>
   );
 };
