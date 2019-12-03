aa4b23d1b5a1f4cdb5ad9a847d4735fa82674ebe
Edwards97 <42946380+Edwards97@users.noreply.github.com>
2019-12-02 20:40:10 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ const App = () => {
   return (
     <div className="App">
       <TitleBar />
+          <CSVImport />
           <SheetJSApp />
           <RequestServer />
           <FileUpload />
