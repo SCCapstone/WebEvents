178572825251fd23392486537b53786c970bb7d5
Jesse E <32375053+jestry@users.noreply.github.com>
2019-12-05 04:00:01 -0500

    troubleshooting

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import CSVImport from "./CSVImport";
 import TitleBar from "./TitleBar";
@@ -7,7 +7,33 @@ import RequestServer from "./RequestServer";
 import FileUpload from "./FileUpload";
 import Options from "./Options";
 
-const App = () => {
+function App {
+  const [message, setMessage] = useState(null);
+  const [isFetching, setIsFetching] = useState(false);
+  const [url, setUrl] = useState('/api');
+
+  const fetchData = useCallback(() => {
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`status ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(json => {
+        setMessage(json.message);
+        setIsFetching(false);
+      }).catch(e => {
+        setMessage(`API call failed: ${e}`);
+        setIsFetching(false);
+      })
+  }, [url]);
+
+  useEffect(() => {
+    setIsFetching(true);
+    fetchData();
+  }, [fetchData]);
+
   return (
     <div className="App">
       <TitleBar />
