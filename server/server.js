7444522f7cc3dd909cc32ce9686253beb49d57b6
Ubuntu <ubuntu@ip-172-31-27-160.us-east-2.compute.internal>
2020-02-24 18:11:39 +0000

    server update, not broken

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-var multer = require('multer')
+var multer = require('multer');
 var cors = require('cors');
 const app = express();
 app.use(cors())
-function uploadPost() {
+//function uploadPost() {
   var storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, 'public')
@@ -21,6 +21,7 @@ function uploadPost() {
       } else if (err) {
         return res.status(500).json(err)
       }
+	    console.log('users are uploading file to server');
       return res.status(200).send(req.file)
     })
  
@@ -29,7 +30,7 @@ function uploadPost() {
 const http = require("http");
 // hostname was 'localhost'
 const hostname = 'http://ec2-3-133-106-204.us-east-2.compute.amazonaws.com';
-const port = 3000;
+const port = 8000;
  
 //Create HTTP server and listen on port 3000 for requests
 const server = http.createServer((req, res) => {
@@ -41,15 +42,15 @@ const server = http.createServer((req, res) => {
 });
  
 //listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, function()  {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+//server.listen(port, function()  {
+//  console.log(`Server running at http://${hostname}:${port}/`);
+//});
 //end mozilla docs
  
 //port to run on - if you change this change the proxy
 //const port = process.env.PORT || 5000;
 //console log for running port
-//app.listen(port, () => console.log('Listening on port 5000'));
+app.listen(port, () => console.log('Listening on port 8000'));
  
 //GET route that wwill be fetched from react
 //the REACT(not server) application needds a proxy for this
@@ -58,3 +59,10 @@ app.get('/express_backend', (req, res) => {
 	console.log('express_backend called');
   res.send({ express: 'Your express backend is connected to react' });
 });
+app.get('/download', function(req, res){
+	console.log('user downloading file from server');
+  const file = `/home/ubuntu/WebEvents/WebEvents/server/public/1582482911741-test.ical`;
+  res.download(file); // Set disposition and send it.
+});
+
+
