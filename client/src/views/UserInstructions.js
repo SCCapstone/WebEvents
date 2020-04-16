4298de4971ff8de544fd6732424e530e5a3830c0
shmiggy1010 <shmiggy1010@gmail.com>
2020-04-16 17:42:39 -0400

    made userInstructions into a list

--- client/src/views/UserInstructions.js
+++ client/src/views/UserInstructions.js
@@ -1,54 +1,69 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "../CSS/App.css";
 import "../CSS/Home.css";
 
 class Instructions extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-          navbarClass: "instructions"
-        }
-      }
-    
-    render() {
-        return (
-            <div id="MainBody">
-              <div class="intro-banner img-cont">
-                <img
-                  class="object-fit_cover"
-                  src="Banner3.jpg"
-                  alt="home-major-image-1"
-                />
-              </div>
-            
-               <div class="MainBody-inner">
-                  <div id="padded-container-center">
+  constructor(props) {
+    super(props);
+    this.state = {
+      navbarClass: "instructions",
+    };
+  }
 
-                  <h1>Instructions how to use Web Events</h1>
-                    <h4>How to use:</h4>
-                    <p>
+  render() {
+    return (
+      <div id="MainBody">
+        <div class="intro-banner img-cont">
+          <img
+            class="object-fit_cover"
+            src="Banner3.jpg"
+            alt="home-major-image-1"
+          />
+        </div>
 
-                     1. Select the type of schedule you would like to create, (either Seminar, Field, or Work) from the 
-                        ScheduleOptions box in the top left.<br/>
-                     2. input the needed variables of the selected schedule type in the sidebar to the left.<br/>
-                     3. upload a data file for the schedule you would like to make.<br/>
+        <div class="MainBody-inner">
+          <div id="padded-container-center">
+            <h1>Instructions how to use Web Events</h1>
+            <h4>How to use:</h4>
+            <ol>
+              <li>
+                Select the type of schedule you would like to create, (either
+                Seminar, Field, or Work) from the ScheduleOptions box in the top
+                left.
+              </li>
+              <li>
+                Input the needed variables of the selected schedule type in the
+                sidebar to the left.
+              </li>
+              <li>
+                upload a data file for the schedule you would like to make.
+                <ul>
+                  <ul>-file types supported are CSV, XLSX, and ICS</ul>
+                  <ul>
+                    -seminars format rows are first and last name, the columns
+                    are dates and/or times available, and the data must be list
+                    of priorities
+                  </ul>
+                  <ul>
+                    -fields format rows are the team names, and the columns are
+                    dates and/or times the field is open, and the data must be a
+                    list of priorities.
+                  </ul>
+                </ul>
+              </li>
+              <li>
+                Once the calculation is finished your optimal schedule may be
+                downloaded as a new file.
+              </li>
+            </ol>
 
-                     <nbsp/> -file types supported are CSV, XLSX, and ICS<br/>
-                     <nbsp/>-seminars format rows are first and last name, the columns are dates and/or times available, 
-	                          and the data must be list of priorities<br/>
-                     <nbsp/>-fields format rows are the team names, and the columns are dates and/or times the field is 
-
-	                          open, and the data must be a list of priorities.<br/>
-                     4. Once the calculation is finished your optimal schedule may be downloaded as a new file.
-                      </p>
-
-                    <div className="column-padding"></div>
-                  </div>
-               </div>
-            <div id="MainBody-Right"></div>
-            </div>
-        );
-    }  
+            <div className="column-padding"></div>
+          </div>
+        </div>
+        <div id="MainBody-Right"></div>
+      </div>
+    );
+  }
 }
- 
+
 export default Instructions;
