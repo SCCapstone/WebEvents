de2b2fdae058357db3389bcba00e82da4307d8f3
shmiggy1010 <shmiggy1010@gmail.com>
2020-04-21 19:54:18 -0400

    Instructions formatting

--- client/src/views/UserInstructions.js
+++ client/src/views/UserInstructions.js
@@ -50,28 +50,28 @@ class Instructions extends Component {
                 scheduler selected from the dropdown menu.
                 <ul>
                   <ul>
-                    -File types supported are .csv, .xls, .xlsx, .xlsm, .xltx,
-                    xltm{" "}
+                    &emsp;-File types supported are .csv, .xls, .xlsx, .xlsm,
+                    .xltx, xltm{" "}
                   </ul>
                   <ul>
-                    -Seminar format is: Starting rows are first and last name
-                    followed by preferences. The columns are dates and/or times
-                    available, and the data must be list of priorities going
-                    from 1 as top priority to higher numbers being lower
+                    &emsp;-Seminar format is: Starting rows are first and last
+                    name followed by preferences. The columns are dates and/or
+                    times available, and the data must be list of priorities
+                    going from 1 as top priority to higher numbers being lower
                     priority.
                   </ul>
                   <ul>
-                    -Field format is: rows are the team names, and the columns
-                    are dates and/or times the field is open, and the data must
-                    be a list of priorities with 1 being top priority and higher
-                    numbers being lower priority. This scheduling algorithm does
-                    not account for size and will give each person a unique time
-                    according to their preference.
+                    &emsp;-Field format is: rows are the team names, and the
+                    columns are dates and/or times the field is open, and the
+                    data must be a list of priorities with 1 being top priority
+                    and higher numbers being lower priority. This scheduling
+                    algorithm does not account for size and will give each
+                    person a unique time according to their preference.
                   </ul>
                   <ul>
-                    -Work Scheduler format is: rows are the people, and the
-                    columns are the shifts. This scheduling algorithm takes in
-                    the size that you want for each shift, and will try to
+                    &emsp;-Work Scheduler format is: rows are the people, and
+                    the columns are the shifts. This scheduling algorithm takes
+                    in the size that you want for each shift, and will try to
                     assign everyone an optimal work schedule.
                   </ul>
                 </ul>
