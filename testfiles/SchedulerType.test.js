a5895e9c5f90d8349ce7cfb32920feace3547584
lamn18 <lamn@cec.sc.edu>
2020-04-26 13:40:10 -0400

    2020-04-26 1340

--- testfiles/SchedulerType.test.js
+++ testfiles/SchedulerType.test.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import SchedulerOptions from './SchedulerOptions';
+import SchedulerType from '../client/src/components/SchedulerType';
 import ReactDOM from 'react-dom';
 import { create } from "react-test-renderer";
 
 //makes sure that the app renders without crashing
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<SchedulerOptions />, div);
+    ReactDOM.render(<SchedulerType />, div);
     ReactDOM.unmountComponentAtNode(div);
 })
 //makes sure that the sheet component matches the snapshot
 describe("sheets component", () => {
     test("Matches the snapshot", () => {
-        const options = create(<SchedulerOptions />);
-        expect(options.toJSON()).toMatchSnapshot();
+        const type = create(<SchedulerType />);
+        expect(type.toJSON()).toMatchSnapshot();
     });
 });
 ;
