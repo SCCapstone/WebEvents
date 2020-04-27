d71baf0fd89ca699441ab7d73524c31ccf7563b8
shmiggy1010 <shmiggy1010@gmail.com>
2020-04-27 14:10:59 -0400

    testing refactor and additions

--- client/src/components/SchedulerOptions.test.js
+++ client/src/components/SchedulerOptions.test.js
@@ -1,19 +1,18 @@
 import React from "react";
+import SchedulerOptions from "./SchedulerOptions";
 import ReactDOM from "react-dom";
-import SheetJSApp from "./components/sheet.js";
-import App from "./App";
 import { create } from "react-test-renderer";
 
 //makes sure that the app renders without crashing
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<SchedulerOptions />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 //makes sure that the sheet component matches the snapshot
 describe("sheets component", () => {
   test("Matches the snapshot", () => {
-    const sheet = create(<SheetJSApp />);
-    expect(sheet.toJSON()).toMatchSnapshot();
+    const options = create(<SchedulerOptions />);
+    expect(options.toJSON()).toMatchSnapshot();
   });
 });
